f0c8faa1b84eb482712e50cc82bac2fa70d9f9d8
ayushi-modi <ayushi.s.modi@gmail.com>
2021-06-20 18:19:52 +0530

    Sorted an Array with the help of Bucket Sorting

---
+++ strings/bucket_sort.py
@@ -0,0 +1,37 @@
+# Python3 program to sort an array using bucket sort
+def insertionSort(b):
+	for i in range(1, len(b)):
+		up = b[i]
+		j = i - 1
+		while j >= 0 and b[j] > up:
+			b[j + 1] = b[j]
+			j -= 1
+		b[j + 1] = up	
+	return b	
+			
+def bucketSort(x):
+	arr = []
+	slot_num = 10 
+	for i in range(slot_num):
+		arr.append([])
+		
+	for j in x:
+		index_b = int(slot_num * j)
+		arr[index_b].append(j)
+	
+	for i in range(slot_num):
+		arr[i] = insertionSort(arr[i])
+		
+	k = 0
+	for i in range(slot_num):
+		for j in range(len(arr[i])):
+			x[k] = arr[i][j]
+			k += 1
+	return x
+
+#test
+x = [0.897, 0.565, 0.656,
+	0.1234, 0.665, 0.3434]
+print("Unsorted Array is:", x)
+print("Sorted Array is")
+print(bucketSort(x))
